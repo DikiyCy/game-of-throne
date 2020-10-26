ce15292863f2e3f7d08bbab027432cd5f960b8ed
Mike_S_D <9834088@gmail.com>
2020-10-26 23:21:07 +0300

    Updates

---
+++ precache-manifest.3f5fe9f7659a4ba86a01616724249dfa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f8862c92221259fe667ceeda6ae54d4",
+    "url": "/game-of-throne/index.html"
+  },
+  {
+    "revision": "7a9a8d2d29d22b927787",
+    "url": "/game-of-throne/static/css/2.80dce9aa.chunk.css"
+  },
+  {
+    "revision": "c8df6e2f2e72769d4d89",
+    "url": "/game-of-throne/static/css/main.746d4d77.chunk.css"
+  },
+  {
+    "revision": "7a9a8d2d29d22b927787",
+    "url": "/game-of-throne/static/js/2.38a042c3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/game-of-throne/static/js/2.38a042c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8df6e2f2e72769d4d89",
+    "url": "/game-of-throne/static/js/main.c92a5e3b.chunk.js"
+  },
+  {
+    "revision": "628ede8104b94e92a7b2",
+    "url": "/game-of-throne/static/js/runtime-main.f43deac5.js"
+  },
+  {
+    "revision": "22389cf47ad284464c13506f2e22e8af",
+    "url": "/game-of-throne/static/media/error.22389cf4.jpg"
+  }
+]);
